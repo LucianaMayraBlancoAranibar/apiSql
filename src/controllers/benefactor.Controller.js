7a99d88d0a4f31729ae8d7b03082a01aa990e4b2
LucianaMayraBlancoAranibar <bal0029274@est.univalle.edu>
2023-06-09 09:04:51 -0400

    .

--- src/controllers/benefactor.Controller.js
+++ src/controllers/benefactor.Controller.js
@@ -19,9 +19,9 @@ export const getBenefactor = async (req,res) =>{
      res.send(result.recordset[0]);
   };
   export const createNewBenefactor = async (req, res) => {
-    const { nombres, apellidos,carnet,dirreccion,email} = req.body;
+    const { nombres, apellidos,segundoApellido,carnet,usuario,contraseña,dirreccion,email} = req.body;
 
-    let {latitud,longitud,telefono,celular } = req.body;
+    let {celular } = req.body;
     
     if (nombres == null || carnet == null) {
       return res.status(400).json({ msg: "Bad Request. Please fill all fields" });
@@ -34,16 +34,16 @@ export const getBenefactor = async (req,res) =>{
         .request()
         .input("nombres", sql.VarChar, nombres)
         .input("apellidos", sql.VarChar, apellidos)
+        .input("segundoApellido", sql.VarChar, segundoApellido)
         .input("carnet", sql.VarChar, carnet)
-        .input("latitud", sql.Float, latitud)
-        .input("longitud", sql.Float, longitud)
+        .input("usuario", sql.VarChar, usuario)
+        .input("contraseña", sql.VarChar, contraseña)
         .input("dirreccion", sql.VarChar, dirreccion)
         .input("email", sql.VarChar, email)
-        .input("telefono", sql.Int, telefono)
         .input("celular", sql.Int, celular)
-        .query("INSERT INTO [AsilosAncianos].[dbo].[Benefactor] (nombres, apellidos, carnet,latitud,longitud,dirreccion,email,telefono,celular) VALUES (@nombres,@apellidos,@carnet,@latitud,@longitud,@dirreccion,@email,@telefono,@celular);");
+        .query("INSERT INTO [AsilosAncianos].[dbo].[Benefactor] (nombres, apellidos, carnet,usuario,contraseña,dirreccion,email,celular) VALUES (@nombres,@apellidos,@carnet,@usuario,@contraseña,@dirreccion,@email,@celular);");
   
-      res.json({ nombres, apellidos, carnet,latitud,longitud,dirreccion,email, telefono,celular});
+      res.json({ nombres, apellidos, carnet,usuario,contraseña,dirreccion,email,celular});
     } catch (error) {
       res.status(500);
       res.send(error.message);
@@ -69,11 +69,11 @@ export const getBenefactor = async (req,res) =>{
   };
   
   export const updateBenefactorById = async (req, res) => {
-    const {nombres, apellidos,carnet,direccion,email,latitud,longitud,telefono,celular} = req.body;
+    const {nombres, apellidos,carnet,direccion,email,usuario,contraseña,celular} = req.body;
     
     const {id}=req.params;
   
-    if (nombres == null || carnet == null ||  apellidos == null || latitud == null || longitud == null || direccion == null  || email==null || telefono==null || celular==null ) {
+    if (nombres == null || carnet == null ||  apellidos == null || usuario == null || contraseña == null || direccion == null  || email==null || celular==null ) {
       return res.status(400).json({ msg: "Bad Request. Please fill all fields" });
     }
   
@@ -84,14 +84,13 @@ export const getBenefactor = async (req,res) =>{
         .input("nombres", sql.VarChar, nombres)
         .input("apellidos", sql.VarChar, apellidos)
         .input("carnet", sql.VarChar, carnet)
-        .input("latitud", sql.Float, latitud)
-        .input("longitud", sql.Float, longitud)
+        .input("usuario", sql.Float, latitud)
+        .input("contraseña", sql.Float, longitud)
         .input("direccion", sql.VarChar, direccion)
         .input("email", sql.VarChar, email)
-        .input("telefono", sql.Int, telefono)
         .input("celular", sql.Int, celular)
         .input("id", sql.Int,id)
-        .query("UPDATE [AsilosAncianos].[dbo].[Benefactor] SET nombres = @nombres, apellidos = @apellidos, carnet = @carnet , latitud = @latitud ,longitud = @longitud , direccion = @direccion, email = @email,telefono = @telefono,celular = @celular WHERE Id = @id");
+        .query("UPDATE [AsilosAncianos].[dbo].[Benefactor] SET nombres = @nombres, apellidos = @apellidos, carnet = @carnet , usuario = @usuario ,contraseña = @contraseña , direccion = @direccion, email = @email,celular = @celular WHERE Id = @id");
       res.json({nombres, apellidos,carnet,latitud,longitud,direccion,email,telefono,celular });
     } catch (error) {
       res.status(500);
